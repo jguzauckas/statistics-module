4624ab0cec21dfa7f4ed4960f8ba35496786f9f9
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2022-12-14 07:52:31 -0500

    Added files

---
+++ classwork.py
@@ -0,0 +1,6 @@
+# Create a list of 100 random integers between 0 and 50
+# Use a loop!!!!
+
+
+# Use the statistics module to calculate the mean, median, and mode
+# of the list of random integers with nice outputs, i.e. "The mean is ___"
